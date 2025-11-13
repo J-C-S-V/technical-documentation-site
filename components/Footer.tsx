a15134f672c0f3a98fa0a20c6d227c82324623e1
Juan Sanchez <juansan1993@hotmail.com>
2025-11-12 22:44:18 -0500

    Format footer content for improved readability

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="w-full border-t border-slate-200/60 dark:border-slate-800/50 py-6">
-      <div className="container text-sm text-slate-600 dark:text-slate-400">© {new Date().getFullYear()} DocsKit. All rights reserved.</div>
+      <div className="container text-sm text-slate-600 dark:text-slate-400">
+        © {new Date().getFullYear()} DocsKit. All rights reserved.
+      </div>
     </footer>
   );
 }
