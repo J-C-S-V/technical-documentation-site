a73c470f259aa9c4866959c7d52836164c67123d
Juan Sanchez <juansan1993@hotmail.com>
2025-11-24 18:13:53 -0500

    Add loading component and skeleton

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,7 @@
-export default function Dashboard() {
+// import { fetchRevenue } from "../lib/data";
+
+export default async function Dashboard() {
+  // const revenue = await fetchRevenue();
+  // console.log("Total Revenue:", revenue);
   return <h1>Dashboard Page</h1>;
 }
